2dfef14aa53c48204fb8967c076f0385a3e36ce8
Rishit Nagar <rishitnagar87@gmail.com>
2021-08-26 20:33:23 +0530

    java

--- programs/array/sum.java
+++ programs/array/sum.java
@@ -4,7 +4,7 @@ public class sum {
     public static void main(String[] args) {
         Scanner ob = new Scanner(System.in);
 
-        int s;
+        int s = 0;
 
         int Arr[] = new int[6];
         for (int i = 0; i < Arr.length; i++)
@@ -18,5 +18,6 @@ public class sum {
             s = s + Arr[j];
         }
         System.out.println(s);
+        System.out.println(s / Arr.length);
     }
 }
