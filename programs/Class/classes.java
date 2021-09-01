62a198bf6e8fad39f05f1b7401fbaf2c0d38538c
Rishit Nagar <rishitnagar87@gmail.com>
2021-09-01 21:38:03 +0530

    java

--- programs/Class/classes.java
+++ programs/Class/classes.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 class classes {
-    
+    int a = 0;
+    classes(int a)
+    {
+        this.a = a;
+        // System.out.println(this.a);
+    }
+
+    void func()
+    {
+        System.out.println("a = " + a);
+    }
+
+    public static void main(String[] args) 
+    {
+        Scanner input = new Scanner(System.in);
+        int b = input.nextInt();
+        classes ob = new classes(b);
+        System.out.println();
+        ob.func();
+    }
 }
